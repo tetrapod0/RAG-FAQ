900389e8f45a5bfec9095bb35144b053d6abaa68
TDAT-PC\User <tdat5415@gmail.com>
2024-07-06 13:20:03 +0900

    update

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from langchain_core.messages import ChatMessage, HumanMessage, AIMessage, SystemMessage
-from langchain_core.embeddings import Embeddings
+# from langchain_core.embeddings import Embeddings
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
@@ -10,41 +10,62 @@ from langchain_community.vectorstores.utils import DistanceStrategy
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 
 from langserve.pydantic_v1 import BaseModel, Field
 from typing import List, Union
 
 from openai import OpenAI
+from operator import itemgetter
 from typing import List
 
 import streamlit as st
+import time
 import json
 import os
 
 
+# BASE_URL = "http://localhost:1234/v1",
+# API_KEY = "lm-studio",
+# MODEL = "lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF",
+BASE_URL = "https://api.openai.com/v1"
+API_KEY = os.environ["OPENAI_API_KEY"]
+MODEL = "gpt-3.5-turbo"
+
+
 TITLE = "메이플 FAQ 문의 채팅"
 
-RAG_PROMPT_TEMPLATE = "You always answer into Korean. You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise. \nContext: {context} \nQuestion: {question} (You must answer in Korean.) \nAnswer:"
-SYS_PROMPT = "You are a helpful, smart, kind, and efficient AI assistant. You always fulfill the user's requests to the best of your ability. You always answer succinctly. You must always answer in Korean."
+SYS_PROMPT = """
+당신은 똑똑하고 친절한 '메이플스토리 게임의 FAQ 채팅 상담가'입니다.
+"""
+
+PROMPT1 = """
+Question : {question}
+
+당신은 Question에 대해 검색이 필요한 경우 '!검색'이라고 외칩니다.
+그렇지 않은 경우 '!일반'이라고 외칩니다.
+그 외 아무것도 하지 않습니다.
+"""
+
+PROMPT2 = """
+Question : {question}
+
+Question을 검색할 문구로 변환하여 출력하시오.
+"""
+
+RAG_PROMPT_TEMPLATE = """
+검색결과:
+{result}
+
+사용자 질문:
+{question}
+
+검색결과로 사용자 질문에 충분히 답변할 수 있는 경우에만 답변하시오. 
+답변하기 충분하지 않은 경우 사용자에게 더 자세한 질문을 요청하세요.
+"""
 
 
-with open("data.json", 'r', encoding='utf-8') as f:
-    data_dic = json.load(f)
-
-
-class MyEmbeddings(Embeddings):
-    def __init__(self, base_url, api_key="lm-studio"):
-        self.client = OpenAI(base_url=base_url, api_key=api_key)
-
-    def embed_documents(self, texts: List[str], model="nomic-ai/nomic-embed-text-v1.5-GGUF") -> List[List[float]]:
-        texts = list(map(lambda text:text.replace("\n", " "), texts))
-        datas = self.client.embeddings.create(input=texts, model=model).data
-        return list(map(lambda data:data.embedding, datas))
-        
-    def embed_query(self, text: str) -> List[float]:
-        return self.embed_documents([text])[0]
 
 
 def format_docs(docs):
@@ -64,42 +85,45 @@ def format_docs(docs):
 st.set_page_config(page_title=TITLE)
 
 
+if 'data_dic' not in st.session_state:
+    with open("data.json", 'r', encoding='utf-8') as f:
+        st.session_state['data_dic'] = json.load(f)
+data_dic = st.session_state['data_dic']
+
+
 # 언어모델
-if 'llm' not in st.session_state:
-    with st.spinner("Loading LLM..."):
+with st.spinner("Loading LLM..."):
+    if 'llm' not in st.session_state:
         st.session_state['llm'] = ChatOpenAI(
-            base_url="http://localhost:1234/v1",
-            api_key="lm-studio",
-            model="lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF",
-            temperature=0.1,
+            base_url=BASE_URL,
+            api_key=API_KEY,
+            model=MODEL,
+            temperature=0.0,
         )
 llm = st.session_state['llm']
 
 
 # 임베딩 모델
-if 'emb' not in st.session_state:
-    with st.spinner("Loading LLM..."):
-        st.session_state['emb'] = MyEmbeddings(base_url="http://localhost:1234/v1")
+with st.spinner("Loading LLM..."):
+    if 'emb' not in st.session_state:
+        st.session_state['emb'] = OpenAIEmbeddings(
+            base_url=BASE_URL,
+            api_key=API_KEY,
+        )
 emb = st.session_state['emb']
 
-# 보이는 채팅 이력
-if "visible_messages" not in st.session_state:
-    st.session_state["visible_messages"] = [
+# 채팅 이력
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        # ChatMessage(role="assistant", content="저는 FAQ 채팅 상담가입니다. 어떤 문의가 필요하신가요?"),
         ("assistant", "저는 FAQ 채팅 상담가입니다. 어떤 문의가 필요하신가요?"),
     ]
-visible_messages = st.session_state["visible_messages"]
-
-# 숨겨진 채팅 이력
-if "hidden_messages" not in st.session_state:
-    st.session_state["hidden_messages"] = [
-        ("assistant", "저는 FAQ 채팅 상담가입니다. 어떤 문의가 필요하신가요?"),
-    ]
-hidden_messages = st.session_state["hidden_messages"]
+messages = st.session_state["messages"]
 
 
 # 파일 -> 벡터저장소
-if 'retriever' not in st.session_state:
-    with st.spinner("Loading FAQ data..."):
+with st.spinner("Loading FAQ data..."):
+    if 'retriever' not in st.session_state:
         # docs = [Document(page_content=title, metadata={"path":value['metadata']}) 
         #         for title, value in data_dic.items()]
         docs = [title for title in data_dic]
@@ -122,44 +146,43 @@ if 'retriever' not in st.session_state:
 retriever = st.session_state['retriever']
 
 
-# # chat 체인
-if 'chat_chain' not in st.session_state:
+
+# van_chain
+if 'van_chain' not in st.session_state:
     prompt = ChatPromptTemplate.from_messages([
-        # ("system", SYS_PROMPT),
-        ("system", """
-        너는 FAQ 채팅 상담가이다.
-        [검색된 FAQ 리스트]를 참고하여 metadata를 포함하여 사용자 질문에 답변해라.
-        특정 주제나 문제와 관련이 없는 일반적인 질문인 경우, 답변하지 말것.
-        그리고 한국어를 사용해라.
-        """),
+        ("system", SYS_PROMPT + " 관련되지 않은 질문은 받지 않습니다."),
         MessagesPlaceholder(variable_name='messages1'),
-        ("user", """
-        내 질문에 한국어로 답변해줘.
-        Question : {question}
-        """)
+        ("user", "{question}"),
     ])
-    st.session_state['chat_chain'] = prompt | llm | StrOutputParser()
-chain = st.session_state['chat_chain']
+    st.session_state['van_chain'] = prompt | llm | StrOutputParser()
+van_chain = st.session_state['van_chain']
 
-# # chat 체인
-# if 'chat_chain' not in st.session_state:
-#     prompt = ChatPromptTemplate.from_messages([
-#         ("system", SYS_PROMPT),
-#         MessagesPlaceholder(variable_name='messages1'),
-#     ])
-#     st.session_state['chat_chain'] = prompt | llm | StrOutputParser()
-# chain = st.session_state['chat_chain']
+# con_chain
+if 'con_chain' not in st.session_state:
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", SYS_PROMPT + " 관련되지 않은 질문은 받지 않습니다."),
+        MessagesPlaceholder(variable_name='messages1'),
+        ("user", PROMPT1),
+    ])
+    st.session_state['con_chain'] = prompt | llm | StrOutputParser()
+con_chain = st.session_state['con_chain']
 
-# chat 체인
-# if 'chat_chain' not in st.session_state:
-#     prompt = ChatPromptTemplate.from_messages([
-#         ("system", SYS_PROMPT),
-#         MessagesPlaceholder(variable_name='messages1'),
-#         ("user", RAG_PROMPT_TEMPLATE),
-#     ])
-#     st.session_state['chat_chain'] = prompt | llm | StrOutputParser()
-# chain = st.session_state['chat_chain']
+# rag_chain
+if 'rag_chain' not in st.session_state:
+    prompt1 = ChatPromptTemplate.from_messages([("system", SYS_PROMPT), ("user", PROMPT2),])
+    prompt2 = ChatPromptTemplate.from_messages([("system", SYS_PROMPT), ("user", RAG_PROMPT_TEMPLATE),])
 
+    # ch1 = prompt1 | llm | StrOutputParser() | {"question" : RunnablePassthrough()}
+    
+    st.session_state['rag_chain'] = (
+        prompt1 | llm | StrOutputParser()
+        | {
+            "result" : retriever | format_docs,
+            "question" : RunnablePassthrough(),
+        }
+        | prompt2 | llm | StrOutputParser()
+    )
+rag_chain = st.session_state['rag_chain']
 
 
 ##########################################################################################
@@ -167,53 +190,33 @@ chain = st.session_state['chat_chain']
 # 제목
 st.title(TITLE)
 
-
 # 채팅 내역 출력
-for msg in visible_messages:
+for msg in messages:
     st.chat_message(msg[0]).write(msg[1])
-
 
 # 유저 입력
 if user_input := st.chat_input():
     
     # 채팅 표시
-    st.chat_message('user').write(user_input)
+    st.chat_message("user").write(user_input)
 
     # 조건문
-    # hidden_messages.append(('system', CON_PROMPT))
-    # chain.innoke(hidden_messages)
-    
-    # rag 검색
-    # format_docs = lambda docs:"[검색된 데이터] : \n\n" + "\n\n".join(doc.page_content for doc in docs)
-    rag = retriever | format_docs
-    result = rag.invoke(user_input)
-    
-    # 채팅 저장
-    # visible_messages.append(("user", user_input))
-    hidden_messages.append(("user", result))
-    # hidden_messages.append(("user", user_input))
-        
     with st.chat_message('assistant'):
         bot_out = st.empty()
+        condition = con_chain.invoke({"messages1":messages, "question":user_input})
+        print(condition)
+
+        chain = [van_chain, rag_chain][condition[:3] == "!검색"]
         msg = ''
-        # for t in chain.stream(hidden_messages):
-        for t in chain.stream({
-            "messages1" : hidden_messages,
-            # "context" : result,
-            "question" : user_input,
-        }):
+        for t in chain.stream({"messages1":messages, "question":user_input}):
             msg += t
             bot_out.markdown(msg)
 
+        
+    messages.append(("user", user_input))
+    messages.append(("assistant", msg))
     
-    visible_messages.append(("user", user_input))
-    # hidden_messages.append(("user", result))
-    hidden_messages.append(("user", user_input))
-
-    visible_messages.append(('assistant', msg))
-    hidden_messages.append(('assistant', msg))
     
-
 
 
 
